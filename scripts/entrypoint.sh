1a30a8328de36ad2a47d6e675b4c8522be9287ac
Sérgio Dantas <sergiodnts828@gmail.com>
2026-02-26 15:17:44 -0300

    feat: integrate Gunicorn as the WSGI server and update entrypoint script for improved performance

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,4 +3,13 @@
 set -e
 
 python src/manage.py migrate
-python src/manage.py runserver 0.0.0.0:8000
+
+exec gunicorn --chdir src core.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers 1 \
+  --timeout 60 \
+  --graceful-timeout 45 \
+  --keep-alive 10 \
+  --access-logfile - \
+  --error-logfile - \
+  --log-level info
